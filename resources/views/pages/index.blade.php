82ca4a3c35661479634b6b8a7bea3839c92a5079
Eyþór Máni Steinarsson <eythormani@gmail.com>
2016-03-07 14:41:07 +0000

    I legitimately don't remember

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,13 @@
+<!-- Stored in resources/views/child.blade.php -->
+
 @extends('pages.layout')
 
-@section('main')
-<p>and this is the other page </p>
+@section('title', 'Page Title')
 
-@stop
+@section('sidebar')
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<title>Crowd Movies</title>
-	</head>
-	<body>
-	hey man this is layout 
-	@section('main')
-	</body>
+    <head>
+        <title>CrowdMovies</title>
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+    </head>
+    <body>
+        @include('pages.navigation')
+        @section('sidebar')
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
 </html>
